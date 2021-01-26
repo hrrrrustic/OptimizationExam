a52ca53037e30a46406448ce9e3c3a05499e1a84
Vlad <hrrrrustic@gmail.com>
2021-01-26 08:12:40 +0300

    add errors

--- OptimizationExam/Program.cs
+++ OptimizationExam/Program.cs
@@ -30,6 +30,57 @@ namespace OptimizationExam
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine(matrix.GetInverseMatrix().ToString());
+
+            Console.WriteLine("Matrix with diagonal sum, n and total error");
+            for (int n = 2; n < 30; n++)
+            {
+                var a = Matrix.GetRandomWithDiagonalSum(n);
+                var b = a.LUDecompose();
+                Double[,] c = new double[n, n];
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < n; ++j)
+                    {
+                        c[i, j] = 0;
+                        for (int k = 0; k < n; ++k)
+                            c[i, j] += b.L[i, k] * b.U[k, j];
+                    }
+                }
+
+                Console.WriteLine($"{n} {GetAvgMatrixDifference(n, a, new Matrix(c)):E}");
+            }
+            Console.WriteLine("\nHilbert Matrix, n and total error");
+            for (int n = 2; n < 30; n++)
+            {
+                var a = Matrix.GetHilbertMatrix(n);
+                var b = a.LUDecompose();
+                Double[,] c = new double[n, n];
+                for (int i = 0; i < n; ++i)
+                {
+                    for (int j = 0; j < n; ++j)
+                    {
+                        c[i, j] = 0;
+                        for (int k = 0; k < n; ++k)
+                            c[i, j] += b.L[i, k] * b.U[k, j];
+                    }
+                }
+
+                Console.WriteLine($"{n} {GetAvgMatrixDifference(n, a, new Matrix(c)):E}");
+            }
+        }
+
+        static double GetAvgMatrixDifference(int n, Matrix a, Matrix b)
+        {
+            double ans = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    ans += Math.Abs(a[i, j] - b[i, j]);
+                }
+            }
+
+            return ans;
         }
     }
 }
